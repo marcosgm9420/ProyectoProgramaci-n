3d0ed2dcb2230c1ead9b3dad258fa18b16dcc096
Guillermo Gutiérrez-14 <guillermogutierrezsanchez012@gmail.com>
2026-02-27 13:37:04 +0100

    Commit de inicio del main

--- prg2º_eval/src/main/Main.java
+++ prg2º_eval/src/main/Main.java
@@ -1,5 +1,24 @@
 package main;
 
+import java.util.Scanner;
+
+import juego.Juego;
+
 public class Main {
+	public static void main(String[] args) throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        linea('=', 60);
+        System.out.println(" Juego 2ªEvaluación");
+        linea('=', 60);
+        System.out.println("   NOTA: El Mercenario usa Katana (cuerpo a cuerpo).");
+        System.out.println("   Si no esta cerca del enemigo, no puede usarla.");
+        linea('-', 60);
 
 }
+
+	private static void linea(char c, int i) {
+		// TODO Auto-generated method stub
+		
+	}}
